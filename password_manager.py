9ece9f810ed3965582a585e6677a88f603138efb
Ga3lFr4 <98173438+Ga3lFr4@users.noreply.github.com>
2023-05-26 11:23:53 +0200

    Update password_manager.py

--- password_manager.py
+++ password_manager.py
@@ -118,7 +118,7 @@ website_entry.focus()
 website_entry.grid(column=1, row=1)
 
 email_entry = Entry(width=47)
-email_entry.insert(INSERT, "gael.francoise@gmail.com")
+email_entry.insert(INSERT, "*******")
 email_entry.grid(column=1, row=2, columnspan=2)
 
 password_entry = Entry(width=24)
